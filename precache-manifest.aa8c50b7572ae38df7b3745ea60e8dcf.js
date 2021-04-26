74f8ccb96bc4065bcea9be928954009f8ce0091f
DESKTOP-OBQC0DF\HUNG PC <linh.mhl9@gmail.com>
2021-04-26 11:32:13 +0700

    Updates

---
+++ precache-manifest.aa8c50b7572ae38df7b3745ea60e8dcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a1b3872766215719731b454a372100",
+    "url": "/ShoppingCart/index.html"
+  },
+  {
+    "revision": "91a076de9995e375f5ce",
+    "url": "/ShoppingCart/static/css/2.5dddc201.chunk.css"
+  },
+  {
+    "revision": "7c87e40d1e01f31c2976",
+    "url": "/ShoppingCart/static/css/main.b70579db.chunk.css"
+  },
+  {
+    "revision": "91a076de9995e375f5ce",
+    "url": "/ShoppingCart/static/js/2.159bc1e0.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/ShoppingCart/static/js/2.159bc1e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c87e40d1e01f31c2976",
+    "url": "/ShoppingCart/static/js/main.0c0f39ca.chunk.js"
+  },
+  {
+    "revision": "74337934d7ff087424dc",
+    "url": "/ShoppingCart/static/js/runtime-main.3a569d55.js"
+  }
+]);
